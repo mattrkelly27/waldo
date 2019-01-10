df1133b8f7ed372f3d013c0878e29ca9f651248b
Matt Kelly <matt.r.kelly27@gmail.com>
2019-01-10 20:29:20 +0000

    this is the final draft

--- waldo.js
+++ waldo.js
@@ -1,14 +1,24 @@
 function findWaldo(arr, found) {
   var nameToFind = "Waldo";
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === nameToFind) {
-      found(i, nameToFind);
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === nameToFind) {
+//       found(i, nameToFind);
+//     }
+//   }
+// }
+
+  arr.forEach (function(item, index) {
+    // console.log(item, i);
+    if (item === nameToFind) {
+      found(item, index);
     }
-  }
+  });
 }
 
-function actionWhenFound(index, name) {
+function actionWhenFound(name, index) {
   console.log("Found " + name + " at Index " + index);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
